a50dde0db1fea067b0369015d83f61e861c5c56e
Simakin Dima <dmdmsimakin@gmail.com>
2021-07-28 03:10:16 +0300

    Cells disappear, but new ones do not appear

--- drawLife.py
+++ drawLife.py
@@ -1,14 +1,63 @@
 import pygame
+from collections import namedtuple
+from random import randint, randrange
+
+WIDTH = 800
+HEIGHT = 800
+FPS = 2
+colors = {"WHITE": (255, 255, 255),
+          "GREEN": (0, 255, 0),
+          "BLACK": (0, 0, 0),
+          "BLUE": (0, 0, 255),
+          "RED": (255, 0, 0)
+          }
+
+Cell = namedtuple("Cell", "is_alive number_of_living_cells_around")
+Coord = namedtuple("Coord", "x y")
+living_cells = {Coord(randrange(0, WIDTH, 10), randrange(0, HEIGHT, 10)): Cell(True, 0) for _ in range(randint(3000, 5000))}
+
+
+def new_living_cells(cells: dict):
+    old_cells = cells.copy()
+    for coord in old_cells:
+        for i in range(-10, 20, 10):
+            for j in range(-10, 20, 10):
+                if not i == j == 0:
+                    new_coord = Coord(coord.x + i, coord.y + j)
+                    if new_coord in old_cells.keys():  # if point living
+                        cells[new_coord] = Cell(cells[new_coord].is_alive, cells[new_coord].number_of_living_cells_around + 1)
+                    else:
+                        cells[new_coord] = Cell(False, 1)
+
+    old_cells = cells.copy()
+    for cell in old_cells:
+        if not old_cells[cell].is_alive and old_cells[cell].number_of_living_cells_around != 3 or \
+                old_cells[cell].is_alive and old_cells[cell].number_of_living_cells_around not in (2, 3):
+            cells.pop(cell)
+
+
 
 pygame.init()
-win = pygame.display.set_mode((500, 500)) 	# Field size
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("LIFE")
+clock = pygame.time.Clock()
+screen.fill(colors["WHITE"])
+pygame.display.update()
 
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # check for closing window
+            running = False
 
-run = True
-while run:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			run = False
+    screen.fill(colors["WHITE"])
+    for cell in living_cells:
+        pygame.draw.rect(screen, colors["RED"], (*cell, 10, 10))
+    pygame.display.flip()
+
+    new_living_cells(living_cells)
+    # Держим цикл на правильной скорости
+    clock.tick(FPS)
 
 pygame.quit()
+quit()
