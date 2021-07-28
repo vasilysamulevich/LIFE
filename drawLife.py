36b87e9ff4ab02eb6be5470086f7f5afd7c6ef58
Simakin Dima <dmdmsimakin@gmail.com>
2021-07-28 13:39:18 +0300

    Works, but eyes hurt

--- drawLife.py
+++ drawLife.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 import pygame
 from collections import namedtuple
 from random import randint, randrange
@@ -7,16 +5,16 @@ from random import randint, randrange
 WIDTH = 800
 HEIGHT = 800
 FPS = 10
-colors = {"WHITE": (255, 255, 255),
-          "GREEN": (0, 255, 0),
-          "BLACK": (0, 0, 0),
-          "BLUE": (0, 0, 255),
-          "RED": (255, 0, 0)
-          }
+WHITE = 255, 255, 255
+GREEN = 0, 255, 0
+BLACK = 0, 0, 0
+BLUE = 0, 0, 255
+RED = 255, 0, 0
 
 Cell = namedtuple("Cell", "is_alive number_of_living_cells_around")
 Coord = namedtuple("Coord", "x y")
 living_cells = {Coord(randrange(0, WIDTH, 10), randrange(0, HEIGHT, 10)): Cell(True, 0) for _ in range(randint(3000, 5000))}
+#living_cells = {Coord(x, y): Cell(True, 0) for x in range(300, 400, 10) for y in range(300, 400, 10)}  # КВАДРАТ
 
 
 def new_living_cells(cells: dict):
@@ -27,27 +25,29 @@ def new_living_cells(cells: dict):
                 if not i == j == 0:
                     new_coord = Coord(coord.x + i, coord.y + j)
                     if new_coord in old_cells.keys():  # if point living
-                        cells[new_coord] = Cell(cells[new_coord].is_alive, cells[new_coord].number_of_living_cells_around + 1)
+                        cells[new_coord] = Cell(cells[new_coord].is_alive,
+                                                cells[new_coord].number_of_living_cells_around + 1)
                     else:
                         cells[new_coord] = Cell(False, 1)
 
     old_cells = cells.copy()
     for cell in old_cells:
         if not old_cells[cell].is_alive and old_cells[cell].number_of_living_cells_around == 3:
-            cells[cell] = Cell(True, old_cells[cell].number_of_living_cells_around)
-        if old_cells[cell].is_alive and old_cells[cell].number_of_living_cells_around not in (2, 3):
+            cells[cell] = Cell(True, 0)
+        elif old_cells[cell].is_alive:
+            if old_cells[cell].number_of_living_cells_around not in (2, 3):
+                cells.pop(cell)
+            else:
+                cells[cell] = Cell(True, 0)
+        elif not old_cells[cell].is_alive and old_cells[cell].number_of_living_cells_around != 3:
             cells.pop(cell)
-    for cell in cells:
-        cells[cell] = Cell(True, 0)
-    
-
 
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("LIFE")
 clock = pygame.time.Clock()
-screen.fill(colors["WHITE"])
+screen.fill(WHITE)
 pygame.display.update()
 
 running = True
@@ -56,11 +56,11 @@ while running:
         if event.type == pygame.QUIT:  # check for closing window
             running = False
 
-    screen.fill(colors["BLACK"])
+    screen.fill(BLACK)
     for cell in living_cells:
-        pygame.draw.rect(screen, colors["GREEN"], (*cell, 10, 10))
-    pygame.display.update() #pygame.display.flip()
+        pygame.draw.rect(screen, GREEN, (*cell, 10, 10))
 
+    pygame.display.flip()   # pygame.display.update()
     new_living_cells(living_cells)
     # Держим цикл на правильной скорости
     clock.tick(FPS)
